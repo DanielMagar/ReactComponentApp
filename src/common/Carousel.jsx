519f9160357d2ce265127dfe395502f09f87fa72
Daniel-Magar <daniel.t@ekoinfomatics.com>
2022-07-27 18:31:53 +0530

    carousel

--- src/common/Carousel.jsx
+++ src/common/Carousel.jsx
@@ -1,24 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const CarouselItem = ({ children, width }) => {
   return (
     <>
-      <div style={{ width: width }}>{children}</div>
+      <div
+        className="inline-flex items-center justify-center h-[400px] bg-teal-400 text-white"
+        style={{ width: width }}
+      >
+        {children}
+      </div>
     </>
   );
 };
 const Carousel = ({ children }) => {
   console.log("Children", children);
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  // updating index for updating activeIndex state value to activate translate into 0, 100 and 200 respectively
+  const updateIndex = (newIndex) => {
+    if (newIndex < 0) {
+      newIndex = 0;
+    } else if (newIndex >= React.Children.count(children)) {
+      newIndex = React.Children.count(children) - 1;
+    }
+    setActiveIndex(newIndex);
+  };
   return (
     <>
       <div className="overflow-hidden">
-        <div className="whitespace-nowrap transition-transform duration: 0.3s -translate-x-[0%]">
-          {React.Children.map(
-            (children,
-            (child, idx) => {
-              return React.cloneElement(child, { width: "100%" });
-            })
-          )}
+        <div
+          className={`whitespace-nowrap transition-all duration-[0.3s] -translate-x-[${
+            activeIndex * 100
+          }%]`}
+        >
+          {React.Children.map(children, (child, index) => {
+            return React.cloneElement(child, { width: "100%" });
+          })}
+        </div>
+        <div className="flex items-center justify-center gap-2">
+          <button
+            className="bg-teal-500 p-2 rounded-[50%] text-white"
+            onClick={() => updateIndex(activeIndex - 1)}
+          >
+            <i className="bx bxs-chevron-left"></i>
+          </button>
+          {React.Children.map(children, (child, index) => {
+            return (
+              <button
+                className={`p-2 rounded-[80%] text-white active:bg-purple-500 ${
+                  index === activeIndex ? "bg-purple-500" : "bg-teal-500"
+                }`}
+                onClick={() => updateIndex(index)}
+              >
+                {index + 1}
+              </button>
+            );
+          })}
+          <button
+            className="bg-teal-500 p-2 rounded-[50%] text-white"
+            onClick={() => updateIndex(activeIndex + 1)}
+          >
+            <i className="bx bxs-chevron-right"></i>
+          </button>
         </div>
       </div>
     </>
--- src/components/Rcomp2.jsx
+++ src/components/Rcomp2.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Carousel from "../common/Carousel";
+import Carousel, { CarouselItem } from "../common/Carousel";
 
 const Rcomp2 = () => {
   return (
@@ -7,7 +7,11 @@ const Rcomp2 = () => {
       <div className=" w-full  p-4 rounded">
         <div className=" w-full border  p-4 rounded shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)]">
           <div>
-            <Carousel />
+            <Carousel>
+              <CarouselItem>Item 1</CarouselItem>
+              <CarouselItem>Item 2</CarouselItem>
+              <CarouselItem>Item 3</CarouselItem>
+            </Carousel>
           </div>
         </div>
       </div>
